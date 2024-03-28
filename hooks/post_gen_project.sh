33f265759b83e41cf8f6c688ebab11c36d9e80d6
zhaokefei <zhaokefei@tezign.com>
2024-03-28 16:43:21 +0800

    build: 更改构建工具

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 init() {
-    if ! which rye &> /dev/null
-    then
-        curl -sSf https://rye-up.com/get | bash
-        echo "rye installed"
-    else
-        echo "rye already installed"
-    fi
     git init
-    rye pin 3.11
-    rye sync
+	pip install -U pip -i https://mirrors.tencent.com/pypi/simple
+	pip install pre-commit poetry==1.8.2 -i https://mirrors.tencent.com/pypi/simple
+	pre-commit install
+	pre-commit install --hook-type commit-msg
 }
 
+init_package() {
+    poetry update tzweb --lock -vv
+}
 
 init
+init_package
